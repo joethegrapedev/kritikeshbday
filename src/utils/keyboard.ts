b61f8ab84084499c6b24f2ee822f90657400406e
myong-011 <myong011@e.ntu.edu.sg>
2024-12-06 17:19:13 +0800

    working keyboard??

--- src/utils/keyboard.ts
+++ src/utils/keyboard.ts
@@ -1,1 +1,18 @@
-// need to write some inline keyboard stuff here similar to bonkbot
+import { Keyboard } from "grammy";
+
+export const keyboard = () => {
+  return new Keyboard()
+    .text("Buy").text("Sell & Manage")
+    .row()
+    .text("Help").text("Refer Friends").text("Alerts")
+    .row()
+    .text("Wallet").text("Settings").text("Limit Orders")
+    .row()
+    .text("Pin").text("Refresh")
+    .resized()
+    .persistent();
+};
+function persistent() {
+    throw new Error("Function not implemented.");
+}
+
