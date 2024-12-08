4a6b07861e2e6846071ab5fbfbca42af50f1d982
Luke Tow <lukebroccoli@users.noreply.github.com>
2024-12-08 15:35:12 +0800

    added settings page

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,11 +1,16 @@
 import { Bot } from "grammy";
 import { handleStart } from "./startHandler.ts";
 import { handleMessage } from "./messageHandler.ts";
+import { handleSettings } from "./settingsHandler.ts";
 
 export function registerHandlers(bot: Bot) {
     bot.command("start", handleStart);
 
     bot.command("home", handleStart);
 
+    bot.command("settings", handleSettings);
+
     bot.on("message", handleMessage);
+
+    
 }
---
+++ src/handlers/settingsHandler.ts
@@ -0,0 +1,37 @@
+import { Context } from "grammy";
+import { settingsKeyboard } from "../utils/keyboards";
+
+export async function handleSettings(ctx: Context) {
+
+
+    return ctx.reply(
+        `⚙️ GENERAL \n
+🌐 Language: Switch languages 
+ Min Position: Hide tokens below threshold
+` + `
+🤖 AUTO BUY
+ Quick buy on paste
+` + `
+🎯 BUTTONS
+ Customize buy/sell buttons
+` + `
+📊 SLIPPAGE
+ Set buy/sell slippage
+ Max impact protects in low liquidity
+` + `
+🔒 MEV PROTECT
+ Turbo: Speed + protection when needed
+ Secure: Max protection, might be slower
+` + `
+🔥 TX PRIORITY
+The higher the tip, the faster the transaction lands.
+` + `
+🔐 SELL SAFETY
+Double-confirm 100% sell commands`,
+        {
+            reply_markup: {
+                inline_keyboard: settingsKeyboard().inline_keyboard,
+            }
+        }
+    );
+}
