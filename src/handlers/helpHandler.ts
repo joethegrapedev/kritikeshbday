6a59c353cda4d0ae976ffbed13d2dcc4e57110c9
John <johntowzhichongdev@gmail.com>
2025-02-25 13:15:20 +0800

    Removed manually deleted files

--- src/handlers/helpHandler.ts
+++
@@ -1,5 +0,0 @@
-import { Context } from "grammy";
-
-export function Help(ctx: Context){
-    return ctx.reply("help yourself nigga");
-}
--- src/services/wallet.ts
+++
@@ -1,4 +0,0 @@
-export function getUserBalance() {
-    const balance = 0.000; // query RPC to get up to date information. 
-    return balance;
-}
