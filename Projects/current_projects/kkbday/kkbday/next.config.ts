1498f15f4c58c8b76e31a5c78764ac98176dacd6
John <johntowzhichongdev@gmail.com>
2025-02-25 13:08:31 +0800

    first edition:

---
+++ Projects/current_projects/kkbday/kkbday/next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
+
+
+
+
+
+
